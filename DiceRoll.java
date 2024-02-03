7084df095ee7276f8c11be3b17c64bd7da6f7445
Cesar Gomez <cgomezrc506@gmail.com>
2024-02-02 17:40:33 -0800

    Edited java program to use user input for amount of rounds

--- DiceRoll.java
+++ DiceRoll.java
@@ -1,14 +1,21 @@
 import java.util.Random;
-
+import java.util.Scanner; 
 public class DiceRoll {
     public static void main(String[] args) {
         // Create a random number generator
         Random random = new Random();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter Amount of Rolls ");  
+            int numRolls= sc.nextInt();  
 
-        // Generate a random number between 1 and 6
-        int diceRoll = random.nextInt(6) + 1;
+            // Roll the dice multiple times
+            for (int i = 0; i < numRolls; i++) {
+                // Generate a random number between 1 and 6
+                int diceRoll = random.nextInt(6) + 1;
 
-        // Print the result
-        System.out.println("You rolled a " + diceRoll);
+                // Print the result
+                System.out.println("Roll #" + (i + 1) + ": You rolled a " + diceRoll);
+            }
+        }
     }
 }
