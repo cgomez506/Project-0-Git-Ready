e689db158503626e208099371f6d9038859cab01
Cesar Gomez <cgomezrc506@gmail.com>
2024-02-05 19:04:51 -0800

    shortened

--- DiceRoll.java
+++ DiceRoll.java
@@ -5,7 +5,7 @@ public class DiceRoll {
 
         Random random = new Random();
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter Amount of Rounds you want to play");  
+            System.out.print("Enter Amount of Rounds");  
             int numRolls= sc.nextInt();  
 
             
